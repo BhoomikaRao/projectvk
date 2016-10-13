121c4ad969cc085b3671dcfc1c8ad4de5b0d5d1d
BhoomikaRao <bhoomikarao19@gmail.com>
2016-10-13 14:44:56 +0530

    projectvk

--- app/src/main/java/com/example/bhoomikarrao/projectvk/Main8Activity.java
+++ app/src/main/java/com/example/bhoomikarrao/projectvk/Main8Activity.java
@@ -1,13 +1,52 @@
 package com.example.bhoomikarrao.projectvk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main8Activity extends AppCompatActivity {
+    TextView t2;
+    TextView t3;
+    Button b9;
+    Button b10;
+    RadioGroup rg1;
+    RadioButton rb1,r;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
+        t2=(TextView)findViewById(R.id.textView);
+        t3=(TextView)findViewById(R.id.textView3);
+        b9=(Button)findViewById(R.id.button6);
+        b10=(Button)findViewById(R.id.button7);
+        rg1=(RadioGroup)findViewById(R.id.rg1);
+        rb1=(RadioButton)findViewById(R.id.radioButton6);
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id=rg1.getCheckedRadioButtonId();
+                r=(RadioButton)findViewById(id);
+                String answer=r.getText().toString();
+                Toast.makeText(Main7Activity.this, "correct answer", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        b10=(Button)findViewById(R.id.button6);
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Main7Activity.this,Main9Activity.class);
+                startActivity(in);
+            }
+        });
+
     }
 }
